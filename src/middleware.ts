adcaa7cc9dd7ecbc7641d56aff06cb4f1c8cfff5
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-12-02 15:37:51 -0500

    debug: disable middleware to isolate error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,4 @@
 // src/middleware.ts
-import { auth } from "@/auth"; // Import auth from your NextAuth.js configuration
-
-export default auth((req: any) => {
-  console.log("middleware", req.nextUrl.pathname);
-});
-
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [],
 };
